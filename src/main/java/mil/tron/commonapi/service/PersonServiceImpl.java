97e7c71d310f628a973c683c18e21a6d6889a027
Dylan Nakamoto <dnakamoto.ctr@revacomm.com>
2020-11-30 13:38:27 -1000

    Remove person repository from controller

--- src/main/java/mil/tron/commonapi/service/PersonServiceImpl.java
+++ src/main/java/mil/tron/commonapi/service/PersonServiceImpl.java
@@ -1,61 +1,48 @@
 package mil.tron.commonapi.service;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.UUID;
 
-import javax.persistence.EntityNotFoundException;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import mil.tron.commonapi.person.Person;
-import mil.tron.commonapi.repository.PersonRepository;
 
 @Service
 public class PersonServiceImpl implements PersonService {
-	
-	@Autowired
-	private PersonRepository repository;
-	
 	public PersonServiceImpl() {
-
+		
 	}
+	
+	HashMap<UUID, Person> persons = new HashMap<>();
 
 	@Override
 	public Person createPerson(Person person) {
-		return repository.save(person);
+		persons.put(UUID.randomUUID(), person);
+		
+		return person;
 	}
 
 	@Override
 	public Person updatePerson(UUID id, Person person) {
-		repository.findById(id).ifPresentOrElse(
-				(p1) -> { 
-					repository.save(person);
-				}, 
-				() -> {
-					throw new EntityNotFoundException(String.format("Person with id: %s does not exist", id.toString()));
-				});
+		persons.replace(id, person);
 		
 		return person;
 	}
 
 	@Override
 	public void deletePerson(UUID id) {
-		repository.deleteById(id);
-		
+		persons.remove(id);
 	}
 
 	@Override
 	public Collection<Person> getPersons() {
-		return (Collection<Person>) repository.findAll();
+		return persons.values();
 	}
 
 	@Override
 	public Person getPerson(UUID id) {
-		Person person = repository.findById(id)
-				.orElseThrow(() -> new EntityNotFoundException(String.format("Could not find person by id: %s", id.toString())));
-		
-		return person;
+		return persons.get(id);
 	}
 
 }
