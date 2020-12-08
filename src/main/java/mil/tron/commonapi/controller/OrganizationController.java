c75c0cf7df753d1738e7b2bcb2935c53004b1150
Dylan Nakamoto <dnakamoto.ctr@revacomm.com>
2020-12-08 02:20:55 -1000

    Added initial response codes for Swagger

--- src/main/java/mil/tron/commonapi/controller/OrganizationController.java
+++ src/main/java/mil/tron/commonapi/controller/OrganizationController.java
@@ -16,6 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import mil.tron.commonapi.organization.Organization;
 import mil.tron.commonapi.service.OrganizationService;
 
@@ -29,12 +33,25 @@ public class OrganizationController {
 	}
 	
 	@Operation(summary = "Retrieves all organizations", description = "Retrieves all organizations")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200",
+					description = "Successful operation",
+					content = @Content(schema = @Schema(implementation = Organization.class)))
+	})
 	@GetMapping
 	public ResponseEntity<Collection<Organization>> getOrganizations() {
 		return new ResponseEntity<>(organizationService.getOrganizations(), HttpStatus.OK);
 	}
 	
 	@Operation(summary = "Retrieves an organization by ID", description = "Retrieves an organization by ID")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200",
+					description = "Successful operation",
+					content = @Content(schema = @Schema(implementation = Organization.class))),
+			@ApiResponse(responseCode = "404",
+					description = "Resource not found",
+					content = @Content)
+	})
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Organization> getOrganization(
 			@Parameter(description = "Organization ID to retrieve", required = true) @PathVariable("id") UUID organizationId) {
@@ -47,6 +64,14 @@ public class OrganizationController {
 	}
 	
 	@Operation(summary = "Adds an organization", description = "Adds an organization")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200",
+					description = "Successful operation",
+					content = @Content(schema = @Schema(implementation = Organization.class))),
+			@ApiResponse(responseCode = "400",
+					description = "Bad request",
+					content = @Content)
+	})
 	@PostMapping
 	public ResponseEntity<Organization> createOrganization(
 			@Parameter(description = "Organization to create", required = true) @RequestBody Organization organization) {
@@ -59,6 +84,14 @@ public class OrganizationController {
 	}
 	
 	@Operation(summary = "Updates an existing organization", description = "Updates an existing organization")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200",
+					description = "Successful operation",
+					content = @Content(schema = @Schema(implementation = Organization.class))),
+			@ApiResponse(responseCode = "404",
+					description = "Resource not found",
+					content = @Content)
+	})
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<Organization> updateOrganization(
 			@Parameter(description = "Organization ID to update", required = true) @PathVariable("id") UUID organizationId,
@@ -73,6 +106,11 @@ public class OrganizationController {
 	}
 	
 	@Operation(summary = "Deletes an existing organization", description = "Deletes an existing organization")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "204",
+					description = "Successful operation",
+					content = @Content)
+	})
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Object> deleteOrganization(
 			@Parameter(description = "Organization ID to delete", required = true) @PathVariable("id") UUID organizationId) {
