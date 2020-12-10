bf1435649273cff47362310444383edebced1a91
Dylan Nakamoto <dnakamoto.ctr@revacomm.com>
2020-12-10 09:43:54 -1000

    Remove redundant modifiers from PersonService interface

--- src/main/java/mil/tron/commonapi/service/PersonService.java
+++ src/main/java/mil/tron/commonapi/service/PersonService.java
@@ -5,9 +5,9 @@ import java.util.UUID;
 import mil.tron.commonapi.person.Person;
 
 public interface PersonService {
-	public abstract Person createPerson(Person person);
-	public abstract Person updatePerson(UUID id, Person person);
-	public abstract void deletePerson(UUID id);
-	public abstract Iterable<Person> getPersons();
-	public abstract Person getPerson(UUID id);
+	Person createPerson(Person person);
+	Person updatePerson(UUID id, Person person);
+	void deletePerson(UUID id);
+	Iterable<Person> getPersons();
+	Person getPerson(UUID id);
 }
