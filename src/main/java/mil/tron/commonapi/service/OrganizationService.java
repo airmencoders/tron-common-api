9ff2f4da3de2372405dec8020f82fd8f2a77868d
Dylan Nakamoto <dnakamoto.ctr@revacomm.com>
2020-12-10 09:42:49 -1000

    Remove redundant modifiers from OrganizationService

--- src/main/java/mil/tron/commonapi/service/OrganizationService.java
+++ src/main/java/mil/tron/commonapi/service/OrganizationService.java
@@ -5,9 +5,9 @@ import java.util.UUID;
 import mil.tron.commonapi.organization.Organization;
 
 public interface OrganizationService {
-	public abstract Organization createOrganization(Organization organization);
-	public abstract Organization updateOrganization(UUID id, Organization organization);
-	public abstract void deleteOrganization(UUID id);
-	public abstract Iterable<Organization> getOrganizations();
-	public abstract Organization getOrganization(UUID id);
+	Organization createOrganization(Organization organization);
+	Organization updateOrganization(UUID id, Organization organization);
+	void deleteOrganization(UUID id);
+	Iterable<Organization> getOrganizations();
+	Organization getOrganization(UUID id);
 }
