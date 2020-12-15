d2b5378f97030f6d57ef092185f8a7006f677eb3
Christopher Zell <zellcc@revacomm.com>
2020-12-14 20:55:55 -0500

    removed redundant public abstracts from interfaces

--- src/main/java/mil/tron/commonapi/service/AirmanService.java
+++ src/main/java/mil/tron/commonapi/service/AirmanService.java
@@ -1,16 +1,14 @@
 package mil.tron.commonapi.service;
 
 import mil.tron.commonapi.airman.Airman;
-import mil.tron.commonapi.exception.InvalidRecordUpdateRequest;
-import mil.tron.commonapi.exception.RecordNotFoundException;
 
 import java.util.UUID;
 
 public interface AirmanService {
-    public abstract Airman createAirman(Airman airman);
+    Airman createAirman(Airman airman);
 
-    public abstract Airman updateAirman(UUID id, Airman airman);
-    public abstract void removeAirman(UUID id);
-    public abstract Iterable<Airman> getAllAirman();
-    public abstract Airman getAirman(UUID id);
+    Airman updateAirman(UUID id, Airman airman);
+    void removeAirman(UUID id);
+    Iterable<Airman> getAllAirman();
+    Airman getAirman(UUID id);
 }
