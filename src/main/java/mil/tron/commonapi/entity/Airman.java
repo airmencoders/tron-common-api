cb9d36820fea4c46cabeb814e82e6693541d26b4
Christopher Zell <zellcc@revacomm.com>
2020-12-18 12:22:19 -0500

    [TRONAPI-56] - added airman fields from tempest

--- src/main/java/mil/tron/commonapi/entity/Airman.java
+++ src/main/java/mil/tron/commonapi/entity/Airman.java
@@ -46,4 +46,108 @@ public class Airman extends Person {
     //
     // Putting DODID as a string since using a Long would require manually padding
     //  value in string output if the dodid had leading zeros, this was it stays literal
+
+    @Getter
+    @Setter
+    private String imds;
+
+    /**
+     * Rank field that returns/sets the base class (Person) 'title' field
+     */
+    private String rank;
+
+    public String getRank() {
+        return this.getTitle();
+    }
+
+    public void setRank(String rank) {
+        this.setTitle(rank);
+    }
+
+    @Getter
+    @Setter
+    private String unit;
+
+    /**
+     * Service member's owning Wing
+     */
+    @Getter
+    @Setter
+    private String wing;
+
+    /**
+     * Service member's owning Group
+     */
+    @Getter
+    @Setter
+    private String gp;
+
+    /**
+     * Service member's owning squadron
+     */
+    @Getter
+    @Setter
+    private String squadron;
+
+    @Getter
+    @Setter
+    private String wc;
+
+    @Getter
+    @Setter
+    private String go81;
+
+    /**
+     * Date current rank was obtained (date of rank)
+     */
+    @Getter
+    @Setter
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "UTC")
+    private Date dor;
+
+    /**
+     * Date estimated return from overseas (DEROS)
+     */
+    @Getter
+    @Setter
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "UTC")
+    private String deros;
+
+    @Getter
+    @Setter
+    private String phone;
+
+    @Getter
+    @Setter
+    private String address;
+
+    /**
+     * General purpose flag used by Tempest
+     */
+    @Getter
+    @Setter
+    private boolean admin;
+
+    @Getter
+    @Setter
+    private String fltChief;
+
+    @Getter
+    @Setter
+    private boolean approved;
+
+    @Getter
+    @Setter
+    private String manNumber;
+
+    @Getter
+    @Setter
+    private String dutyPhone;
+
+    /**
+     * Job title performed as an airman
+     */
+    @Getter
+    @Setter
+    private String dutyTitle;
 }
