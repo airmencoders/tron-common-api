248bfccb1fd2aa025ad19055e8d23a1266de0c70
Christopher Zell <zellcc@revacomm.com>
2021-01-01 13:07:42 -0500

    [TRONAPI-38] - removed unused import

--- src/main/java/mil/tron/commonapi/repository/AirmanRepository.java
+++ src/main/java/mil/tron/commonapi/repository/AirmanRepository.java
@@ -3,7 +3,6 @@ package mil.tron.commonapi.repository;
 import mil.tron.commonapi.entity.Airman;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface AirmanRepository extends CrudRepository<Airman, UUID> {
