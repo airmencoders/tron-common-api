53040d0ee1f67632320803a11654b8f59477be16
Christopher Zell <zellcc@revacomm.com>
2021-02-02 15:05:40 -0500

    [TRONAPI-150] - makes the swagger redir value read from properties

--- src/test/java/mil/tron/commonapi/RootPathControllerTest.java
+++ src/test/java/mil/tron/commonapi/RootPathControllerTest.java
@@ -3,6 +3,7 @@ package mil.tron.commonapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -17,11 +18,14 @@ public class RootPathControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Value("${springdoc.swagger-ui.path}")
+    private String swaggerPath;
+
     @Test
     public void testSwaggerRedirect() throws Exception {
 
         mockMvc.perform(get("/"))
                 .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("/api-docs/index"));
+                .andExpect(redirectedUrl(swaggerPath));
     }
 }
