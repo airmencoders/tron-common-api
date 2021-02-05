f6662a23895e2c215a20e891dd03313405b79bc5
Dylan Nakamoto <dnakamoto.ctr@revacomm.com>
2021-02-05 02:50:33 -1000

    Update WebSecurityConfigDisabled.java

--- src/main/java/mil/tron/commonapi/security/WebSecurityConfigDisabled.java
+++ src/main/java/mil/tron/commonapi/security/WebSecurityConfigDisabled.java
@@ -1,16 +1,36 @@
 package mil.tron.commonapi.security;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @ConditionalOnProperty(name = "security.enabled", havingValue="false")
 public class WebSecurityConfigDisabled extends WebSecurityConfigurerAdapter {
 	@Override
-	public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/**");
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+        	.cors()
+        	.and()
+            .csrf()
+        		.disable()
+        	.sessionManagement()
+	        	.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+	
+	@Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
 
