cb8555c14154dbdb5bacb095d0a15acc13f5b01c
Dylan Nakamoto <dnakamoto.ctr@revacomm.com>
2021-02-05 12:05:39 -1000

    Fix cors for local dashboard, liquibase changelogs

--- src/main/java/mil/tron/commonapi/security/WebSecurityConfigDisabled.java
+++ src/main/java/mil/tron/commonapi/security/WebSecurityConfigDisabled.java
@@ -1,14 +1,10 @@
 package mil.tron.commonapi.security;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @ConditionalOnProperty(name = "security.enabled", havingValue="false")
@@ -23,14 +19,4 @@ public class WebSecurityConfigDisabled extends WebSecurityConfigurerAdapter {
         	.sessionManagement()
 	        	.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
-	
-	@Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("*");
-        source.registerCorsConfiguration("/**", config);
-        return source;
-    }
 }
-
