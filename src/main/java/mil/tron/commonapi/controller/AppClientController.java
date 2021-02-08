9d4f7d7f6620310fcad660ab05a575663b92b784
Dylan Nakamoto <dnakamoto.ctr@revacomm.com>
2021-02-08 10:13:07 -1000

    create DTO for AppClientUser

--- src/main/java/mil/tron/commonapi/controller/AppClientController.java
+++ src/main/java/mil/tron/commonapi/controller/AppClientController.java
@@ -6,7 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import mil.tron.commonapi.annotation.security.PreAuthorizeDashboardAdmin;
+import mil.tron.commonapi.dto.AppClientUserDto;
 import mil.tron.commonapi.service.AppClientUserService;
 
 @RestController
@@ -14,12 +20,18 @@ import mil.tron.commonapi.service.AppClientUserService;
 @PreAuthorizeDashboardAdmin
 public class AppClientController {
 	
-	AppClientUserService userService;
+	private AppClientUserService userService;
 	
 	public AppClientController(AppClientUserService userService) {
 		this.userService = userService;
 	}
 	
+	@Operation(summary = "Retrieves all application client user information", description = "Retrieves application client user information")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", 
+				description = "Successful operation", 
+				content = @Content(schema = @Schema(implementation = AppClientUserDto.class)))
+	})
 	@GetMapping
 	public ResponseEntity<Object> getUsers() {
 		return new ResponseEntity<>(userService.getAppClientUsers(), HttpStatus.OK);
