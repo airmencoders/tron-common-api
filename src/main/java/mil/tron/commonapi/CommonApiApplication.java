aad5a3a72ec09c1ad24923de9e8ae41a57fa9ec9
Dylan Nakamoto <dnakamoto.ctr@revacomm.com>
2021-02-09 09:37:43 -1000

    Separate Swagger documentation to groups

--- src/main/java/mil/tron/commonapi/CommonApiApplication.java
+++ src/main/java/mil/tron/commonapi/CommonApiApplication.java
@@ -3,6 +3,8 @@ package mil.tron.commonapi;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -36,5 +38,24 @@ public class CommonApiApplication {
         loggingFilter.setIncludePayload(true);
         loggingFilter.setMaxPayloadLength(64000);
         return loggingFilter;
+    }
+    
+    @Bean
+    public GroupedOpenApi commonApi(@Value("${api-prefix.v1}") String apiPrefix) {
+    	String[] paths = {
+			String.format("%s/person/**", apiPrefix),
+			String.format("%s/organization/**", apiPrefix),
+			String.format("%s/airman/**", apiPrefix),
+		};
+    	return GroupedOpenApi.builder().group("common-api").pathsToMatch(paths).build();
+    }
+    
+    @Bean
+    public GroupedOpenApi dashboardApi(@Value("${api-prefix.v1}") String apiPrefix) {
+    	String[] paths = {
+			String.format("%s/app-client/**", apiPrefix),
+			String.format("%s/userinfo/**", apiPrefix)
+		};
+    	return GroupedOpenApi.builder().group("dashboard-api").pathsToMatch(paths).build();
     }
 }
