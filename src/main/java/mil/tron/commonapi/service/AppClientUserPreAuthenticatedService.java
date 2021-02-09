ac92ff5fd8a2a66ec22b1d6e71f47f5b0a14a3ef
Chad Kumabe <ckumabe.ctr@revacomm.com>
2021-02-09 13:21:14 -1000

    [TRONAPI-157] Add app name in properties for all privileges.

--- src/main/java/mil/tron/commonapi/service/AppClientUserPreAuthenticatedService.java
+++ src/main/java/mil/tron/commonapi/service/AppClientUserPreAuthenticatedService.java
@@ -4,9 +4,14 @@ import java.util.ArrayList;
 
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import javax.transaction.Transactional;
 
+import lombok.val;
+import mil.tron.commonapi.repository.PrivilegeRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
@@ -27,16 +32,34 @@ import mil.tron.commonapi.repository.AppClientUserRespository;
 @Service
 public class AppClientUserPreAuthenticatedService implements AuthenticationUserDetailsService<PreAuthenticatedAuthenticationToken> {
 	
-	private AppClientUserRespository repository;
+	private AppClientUserRespository appClientUserRespository;
+
+	private PrivilegeRepository privilegeRepository;
+
+	@Value("${common-api-app-name}")
+	private String commonApiAppName;
 	
-	public AppClientUserPreAuthenticatedService(AppClientUserRespository repository) {
-		this.repository = repository;
+	public AppClientUserPreAuthenticatedService(AppClientUserRespository appClientUserRespository,
+												PrivilegeRepository privilegeRepository) {
+		this.appClientUserRespository = appClientUserRespository;
+		this.privilegeRepository = privilegeRepository;
 	}
 
 	@Transactional
 	@Override
 	public UserDetails loadUserDetails(PreAuthenticatedAuthenticationToken token) throws UsernameNotFoundException {
-		AppClientUser user = repository.findByNameIgnoreCase(token.getName()).orElseThrow(() -> new UsernameNotFoundException("Username not found: " + token.getName()));
+		// allow for configured self app hostname to have all privileges
+		if (token.getName().equals(this.commonApiAppName)) {
+			val privileges = this.privilegeRepository.findAll();
+			if (privileges == null) {
+				throw new RuntimeException("There are not privileges available.");
+			}
+			// add all privileges for self app
+			Set<Privilege> privilegeSet = StreamSupport.stream(privileges.spliterator(), false)
+					.collect(Collectors.toSet());
+			return new User(this.commonApiAppName, "N/A", this.createPrivileges(privilegeSet));
+		}
+		AppClientUser user = appClientUserRespository.findByNameIgnoreCase(token.getName()).orElseThrow(() -> new UsernameNotFoundException("Username not found: " + token.getName()));
 		List<GrantedAuthority> privileges = createPrivileges(user.getPrivileges());
 
 		return new User(user.getName(), "N/A", privileges);
