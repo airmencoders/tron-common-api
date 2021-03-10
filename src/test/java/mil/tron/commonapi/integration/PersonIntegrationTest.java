c61211bec55c04bd5e1be27400d0050204ee565a
David Milligan <david.milligan@madn.mil>
2021-03-10 16:07:40 -0500

    fix integration test

--- src/test/java/mil/tron/commonapi/integration/PersonIntegrationTest.java
+++ src/test/java/mil/tron/commonapi/integration/PersonIntegrationTest.java
@@ -31,7 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
+@SpringBootTest(properties = { "spring.liquibase.contexts=None" })
 @AutoConfigureMockMvc
 public class PersonIntegrationTest {
     private static final String ENDPOINT = "/v1/person/";
