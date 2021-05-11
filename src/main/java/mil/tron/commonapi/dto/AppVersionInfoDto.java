7a94d809a86eb40db2a788a653da566f9b403f42
Christopher Zell <zellcc@revacomm.com>
2021-05-11 13:03:40 -0400

    [TRONAPI-377] - adds the short commit hash to the /version endpoint

---
+++ src/main/java/mil/tron/commonapi/dto/AppVersionInfoDto.java
@@ -0,0 +1,16 @@
+package mil.tron.commonapi.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Builder
+public class AppVersionInfoDto {
+
+    @Getter
+    @Setter
+    private String version;
+}
