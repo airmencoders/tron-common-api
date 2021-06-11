f5f95b15e953d2910abb58b293e3532301fc8b99
Christopher Zell <zellcc@revacomm.com>
2021-06-11 08:38:29 -0400

    added validation annos

--- src/main/java/mil/tron/commonapi/dto/PlatformJwtDto.java
+++ src/main/java/mil/tron/commonapi/dto/PlatformJwtDto.java
@@ -3,6 +3,10 @@ package mil.tron.commonapi.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+import mil.tron.commonapi.annotation.security.PiiField;
+import mil.tron.commonapi.validations.ValidDodId;
+
+import javax.validation.constraints.Email;
 
 /**
  * Represents the parts we care about from a P1 Istio JWT
@@ -25,20 +29,26 @@ public class PlatformJwtDto {
 
     @Getter
     @Setter
+    @PiiField
+    @Email(regexp = ".+@.+\\..+$", message = "Malformed email address")
     private String email;
 
     @Getter
     @Setter
+    @PiiField
+    @ValidDodId
     @JsonProperty("dod_id")
     private String dodId;
 
     @Getter
     @Setter
+    @PiiField
     @JsonProperty("given_name")
     private String givenName;
 
     @Getter
     @Setter
+    @PiiField
     @JsonProperty("family_name")
     private String familyName;
 }
