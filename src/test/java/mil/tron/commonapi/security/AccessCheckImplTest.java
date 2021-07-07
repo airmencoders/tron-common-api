603631fc51a445cd8afa3a18eb3e4560a3bb74d0
Dylan Nakamoto <dnakamoto.ctr@revacomm.com>
2021-07-07 00:17:09 -1000

    Allow App Client Developers to send requests through app gateway

--- src/test/java/mil/tron/commonapi/security/AccessCheckImplTest.java
+++ src/test/java/mil/tron/commonapi/security/AccessCheckImplTest.java
@@ -6,20 +6,32 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import mil.tron.commonapi.ApplicationProperties;
+import mil.tron.commonapi.entity.AppClientUser;
+import mil.tron.commonapi.entity.DashboardUser;
+import mil.tron.commonapi.entity.Privilege;
+import mil.tron.commonapi.entity.appsource.AppEndpoint;
+import mil.tron.commonapi.entity.appsource.AppEndpointPriv;
+import mil.tron.commonapi.entity.appsource.AppSource;
+import mil.tron.commonapi.service.AppClientUserService;
 
 @SpringBootTest
 public class AccessCheckImplTest {
+	@MockBean
+	private AppClientUserService userService;
     
     @Test
     @WithMockUser(username="guardianangel", authorities = "appsource/endpoint_GET")
@@ -27,7 +39,7 @@ public class AccessCheckImplTest {
         ApplicationProperties prefixProperties = Mockito.mock(ApplicationProperties.class);        
         when(prefixProperties.getCombinedPrefixes()).thenReturn(Arrays.asList("/v1/app"));
 
-        AccessCheckImpl accessCheckImpl = new AccessCheckImpl(prefixProperties);
+        AccessCheckImpl accessCheckImpl = new AccessCheckImpl(prefixProperties, userService);
         HttpServletRequest request = get("/v1/app/appsource/endpoint").buildRequest(null);
         assertTrue(accessCheckImpl.check(request));
     }
@@ -38,7 +50,7 @@ public class AccessCheckImplTest {
         ApplicationProperties prefixProperties = Mockito.mock(ApplicationProperties.class);        
         when(prefixProperties.getCombinedPrefixes()).thenReturn(Arrays.asList("/v1/app", "/v2/app"));
 
-        AccessCheckImpl accessCheckImpl = new AccessCheckImpl(prefixProperties);
+        AccessCheckImpl accessCheckImpl = new AccessCheckImpl(prefixProperties, userService);
         HttpServletRequest request = get("/v2/app/appsource/endpoint").buildRequest(null);
         assertTrue(accessCheckImpl.check(request));
     }
@@ -49,7 +61,7 @@ public class AccessCheckImplTest {
         ApplicationProperties prefixProperties = Mockito.mock(ApplicationProperties.class);        
         when(prefixProperties.getCombinedPrefixes()).thenReturn(Arrays.asList("/v1/app"));
 
-        AccessCheckImpl accessCheckImpl = new AccessCheckImpl(prefixProperties);
+        AccessCheckImpl accessCheckImpl = new AccessCheckImpl(prefixProperties, userService);
         HttpServletRequest request = get("/api/v1/app/appsource/endpoint").buildRequest(null);
         assertTrue(accessCheckImpl.check(request));
     }
@@ -60,8 +72,19 @@ public class AccessCheckImplTest {
         ApplicationProperties prefixProperties = Mockito.mock(ApplicationProperties.class);        
         when(prefixProperties.getCombinedPrefixes()).thenReturn(Arrays.asList("/v1/app"));
         
-        AccessCheckImpl accessCheckImpl = new AccessCheckImpl(prefixProperties);
+        AccessCheckImpl accessCheckImpl = new AccessCheckImpl(prefixProperties, userService);
         HttpServletRequest request = get("/v1/app/appsource/endpoint?test=test").buildRequest(null);
+        assertTrue(accessCheckImpl.check(request));
+    }
+    
+    @Test
+    @WithMockUser(username="guardianangel", authorities = "appsource/endpoint/{pathParam}_GET")
+    public void passAuthenticationCheckWithPathParams() {
+        ApplicationProperties prefixProperties = Mockito.mock(ApplicationProperties.class);        
+        when(prefixProperties.getCombinedPrefixes()).thenReturn(Arrays.asList("/v1/app"));
+        
+        AccessCheckImpl accessCheckImpl = new AccessCheckImpl(prefixProperties, userService);
+        HttpServletRequest request = get("/v1/app/appsource/endpoint/1").buildRequest(null);
         assertTrue(accessCheckImpl.check(request));
     }
 
@@ -71,8 +94,36 @@ public class AccessCheckImplTest {
         ApplicationProperties prefixProperties = Mockito.mock(ApplicationProperties.class);        
         when(prefixProperties.getCombinedPrefixes()).thenReturn(Arrays.asList("/v1/app"));
         
-        AccessCheckImpl accessCheckImpl = new AccessCheckImpl(prefixProperties);
+        AccessCheckImpl accessCheckImpl = new AccessCheckImpl(prefixProperties, userService);
         HttpServletRequest request = get("/v1/app/appsource/endpoint_fail").buildRequest(null);
         assertFalse(accessCheckImpl.check(request));
+    }
+    
+    @Test
+    @WithMockUser(username="test@app.dev", authorities = "APP_CLIENT_DEVELOPER")
+    public void passAuthenticationAsAppClientDeveloper() {
+        ApplicationProperties prefixProperties = Mockito.mock(ApplicationProperties.class);        
+        when(prefixProperties.getCombinedPrefixes()).thenReturn(Arrays.asList("/v1/app"));
+        
+        
+        AppSource appSource = AppSource.builder().appSourcePath("appsource").build();
+        
+        AppClientUser appClient = AppClientUser.builder()
+        		.appClientDevelopers(Set.of(new DashboardUser(UUID.randomUUID(), "test@app.dev", "test@app.dev", Set.of(Privilege.builder().id(1L).name("APP_CLIENT_DEVELOPER").build()))))
+        		.appEndpointPrivs(Set.of(
+        					AppEndpointPriv.builder()
+        						.appEndpoint(AppEndpoint.builder()
+        								.appSource(appSource)
+        								.path("/endpoint")
+        								.method(RequestMethod.GET)
+        								.build())
+        						.build())
+        				)
+        		.build();
+        when(userService.getAppClientUsersContainingDeveloperEmail(Mockito.anyString())).thenReturn(List.of(appClient));
+        
+        AccessCheckImpl accessCheckImpl = new AccessCheckImpl(prefixProperties, userService);
+        HttpServletRequest request = get("/v1/app/appsource/endpoint").buildRequest(null);
+        assertTrue(accessCheckImpl.check(request));
     }
 }
