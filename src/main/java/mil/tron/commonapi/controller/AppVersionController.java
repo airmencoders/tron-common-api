1d38c2ea058d980dcb192f4670edf226fb083e59
Christopher Zell <zellcc@revacomm.com>
2021-09-28 15:56:51 -0400

    moves the enclave fetch to postconstruct

--- src/main/java/mil/tron/commonapi/controller/AppVersionController.java
+++ src/main/java/mil/tron/commonapi/controller/AppVersionController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+
 @RestController
 @RequestMapping({"${api-prefix.v1}/version", "${api-prefix.v2}/version"})
 public class AppVersionController {
@@ -21,8 +23,20 @@ public class AppVersionController {
     // get the version from the JAR Manifest (when application is running as a JAR)
     private final String VERSION = CommonApiApplication.class.getPackage().getImplementationVersion();
 
-    @Value("${spring.profiles.active:UNKNOWN}")
+    private static final String UNKNOWN_ERROR = "Unknown";
+
+    @Value("${spring.profiles.active:Unknown}")
     private String environment;
+    private String enclaveLevel;
+
+    @PostConstruct
+    void populateEnclaveValue() {
+        try {
+            this.enclaveLevel = System.getenv("ENCLAVE_LEVEL");
+        } catch (SecurityException | NullPointerException e) {
+            this.enclaveLevel = UNKNOWN_ERROR;
+        }
+    }
 
     @Operation(summary = "Retrieves current running application version, along with the enclave level and environment this instance is running in",
             description = "The version is the first 8-characters of the SHA-1 commit hash of the master branch that this version was compiled from")
@@ -33,19 +47,10 @@ public class AppVersionController {
     })
     @GetMapping("")
     public ResponseEntity<Object> getVersion() {
-
-        String enclaveLevel;
-
-        try {
-            enclaveLevel = System.getenv("ENCLAVE_LEVEL");
-        } catch (SecurityException | NullPointerException e) {
-            enclaveLevel = "UNKNOWN";
-        }
-
         return new ResponseEntity<>(AppVersionInfoDto
                 .builder()
-                .version(VERSION == null ? "Unknown" : VERSION)
-                .enclave(enclaveLevel)
+                .version(VERSION == null ? UNKNOWN_ERROR : VERSION)
+                .enclave(enclaveLevel == null ? UNKNOWN_ERROR : enclaveLevel)
                 .environment(environment)
                 .build(), HttpStatus.OK);
     }
