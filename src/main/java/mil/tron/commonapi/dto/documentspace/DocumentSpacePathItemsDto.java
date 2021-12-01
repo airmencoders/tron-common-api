8426e54f9d362b9bb6a052cfd6f69ac258bd36a8
Dylan Nakamoto <dnakamoto.ctr@revacomm.com>
2021-11-30 18:50:37 -1000

    Update validation constraints for DocumentSpacePathItemsDto

--- src/main/java/mil/tron/commonapi/dto/documentspace/DocumentSpacePathItemsDto.java
+++ src/main/java/mil/tron/commonapi/dto/documentspace/DocumentSpacePathItemsDto.java
@@ -4,8 +4,7 @@ import lombok.*;
 
 import java.util.List;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 /**
  * Used for path related entries
@@ -17,11 +16,11 @@ public class DocumentSpacePathItemsDto {
 
     @Getter
     @Setter
-    @NotBlank
+    @NotNull
     private String currentPath;
 
     @Getter
     @Setter
-    @NotEmpty
+    @NotNull
     private List<String> items;
 }
