eb45e4617871adf2a2318e1b7e0c22c010ed6416
Christopher Zell <zellcc@revacomm.com>
2022-01-26 12:23:57 -0500

    revert to DefaultHttpFirewall

--- src/main/java/mil/tron/commonapi/WebConfig.java
+++ src/main/java/mil/tron/commonapi/WebConfig.java
@@ -1,18 +1,10 @@
 package mil.tron.commonapi;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.security.web.firewall.RequestRejectedHandler;
-import org.springframework.security.web.firewall.StrictHttpFirewall;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.servlet.http.HttpServletResponse;
-import java.util.Arrays;
-import java.util.Objects;
 
 @Configuration
 @Slf4j
@@ -31,32 +23,28 @@ public class WebConfig implements WebMvcConfigurer {
 
     }
 
-    @Bean
-    public StrictHttpFirewall httpFirewall() {
-        StrictHttpFirewall firewall = new StrictHttpFirewall();
-        firewall.setAllowedHeaderNames((header) -> true);
-        firewall.setAllowedHeaderValues((header) -> true);
-        firewall.setAllowedParameterNames((parameter) -> true);
-        firewall.setAllowSemicolon(true);
-        firewall.setAllowedHttpMethods(Arrays.asList(allowedMethods));
-        return firewall;
-    }
+
+//    @Bean
+//    public DefaultHttpFirewall httpFirewall() {
+//        DefaultHttpFirewall firewall = new DefaultHttpFirewall();
+//        return firewall;
+//    }
 
     /**
      * Log to the console any firewall rejected events
      * @return
      */
-    @Bean
-    public RequestRejectedHandler requestRejectedHandler() {
-        return (httpServletRequest, httpServletResponse, e) -> {
-            log.warn(
-                    "request_rejected: remote={}, user_agent={}, request_url={}, exception={}",
-                    httpServletRequest.getRemoteHost(),
-                    httpServletRequest.getHeader(HttpHeaders.USER_AGENT),
-                    httpServletRequest.getRequestURL(),
-                    e.getMessage()
-            );
-            httpServletResponse.sendError(HttpServletResponse.SC_BAD_REQUEST);
-        };
-    }
+//    @Bean
+//    public RequestRejectedHandler requestRejectedHandler() {
+//        return (httpServletRequest, httpServletResponse, e) -> {
+//            log.warn(
+//                    "request_rejected: remote={}, user_agent={}, request_url={}, exception={}",
+//                    httpServletRequest.getRemoteHost(),
+//                    httpServletRequest.getHeader(HttpHeaders.USER_AGENT),
+//                    httpServletRequest.getRequestURL(),
+//                    e.getMessage()
+//            );
+//            httpServletResponse.sendError(HttpServletResponse.SC_BAD_REQUEST);
+//        };
+//    }
 }
