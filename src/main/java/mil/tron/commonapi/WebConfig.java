106ea432cad2d49424d5a26cddd1e547f066535c
Christopher Zell <czell@revacomm.com>
2022-01-26 15:54:00 +0000

    took out the logging of query params and jwt

--- src/main/java/mil/tron/commonapi/WebConfig.java
+++ src/main/java/mil/tron/commonapi/WebConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpHeaders;
-import org.springframework.security.web.firewall.HttpStatusRequestRejectedHandler;
 import org.springframework.security.web.firewall.RequestRejectedHandler;
 import org.springframework.security.web.firewall.StrictHttpFirewall;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -13,6 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.Arrays;
+import java.util.Objects;
 
 @Configuration
 @Slf4j
@@ -34,21 +34,27 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public StrictHttpFirewall httpFirewall() {
         StrictHttpFirewall firewall = new StrictHttpFirewall();
+        firewall.setAllowedHeaderNames((header) -> true);
+        firewall.setAllowedHeaderValues((header) -> true);
+        firewall.setAllowedParameterNames((parameter) -> true);
+        firewall.setAllowSemicolon(true);
         firewall.setAllowedHttpMethods(Arrays.asList(allowedMethods));
         return firewall;
     }
 
+    /**
+     * Log to the console any firewall rejected events
+     * @return
+     */
     @Bean
     public RequestRejectedHandler requestRejectedHandler() {
         return (httpServletRequest, httpServletResponse, e) -> {
             log.warn(
-                    "request_rejected: remote={}, queryParms={}, jwt={}, user_agent={}, request_url={}",
+                    "request_rejected: remote={}, user_agent={}, request_url={}, exception={}",
                     httpServletRequest.getRemoteHost(),
-                    httpServletRequest.getQueryString(),
-                    httpServletRequest.getHeader("authorization"),
                     httpServletRequest.getHeader(HttpHeaders.USER_AGENT),
                     httpServletRequest.getRequestURL(),
-                    e
+                    e.getMessage()
             );
             httpServletResponse.sendError(HttpServletResponse.SC_BAD_REQUEST);
         };
