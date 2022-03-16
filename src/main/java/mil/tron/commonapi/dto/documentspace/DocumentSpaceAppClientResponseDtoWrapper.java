66bc4aad4659c386f1bd48bfc7ed3c6ca9ebf483
Christopher Zell <czell@revacomm.com>
2022-03-15 20:50:28 -0400

    forgot my new files

---
+++ src/main/java/mil/tron/commonapi/dto/documentspace/DocumentSpaceAppClientResponseDtoWrapper.java
@@ -0,0 +1,9 @@
+package mil.tron.commonapi.dto.documentspace;
+
+import mil.tron.commonapi.dto.response.WrappedResponse;
+
+import java.util.List;
+
+public class DocumentSpaceAppClientResponseDtoWrapper extends WrappedResponse<List<DocumentSpaceAppClientResponseDto>> {
+
+}
